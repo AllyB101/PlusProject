4c5c68ac676f9425ae2ae4f3939049bb2fc2e8f7
AllyB <allendebennett@gmail.com>
2022-06-14 18:33:29 -0500

    redid current time
    re-wrote and turned into a function

--- src/script.js
+++ src/script.js
@@ -1,29 +1,33 @@
-let now = new Date();
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
 
-let day = days[now.getDay()];
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
 
-let date = document.querySelector(".date");
-date.innerHTML = `Today is ${day} at ${hours}:${minutes}`;
+// weather descriptions
 
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -36,6 +40,8 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  // weather icon
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
